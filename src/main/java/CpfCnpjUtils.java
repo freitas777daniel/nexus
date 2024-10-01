3f348f2dd7ccddb315f81f086015c24dd9367c7a
Daniel Freitas <danielsfreitas97@gmail.com>
2024-10-01 19:23:15 -0300

    inserido validações de cpf e cnpj

--- src/main/java/CpfCnpjUtils.java
+++ src/main/java/CpfCnpjUtils.java
@@ -13,7 +13,8 @@ public class CpfCnpjUtils {
             "00000000000000", "11111111111111", "22222222222222", "33333333333333", "44444444444444",
             "55555555555555", "66666666666666", "77777777777777", "88888888888888", "99999999999999");
 
-    private CpfCnpjUtils() {}
+    private CpfCnpjUtils() {
+    }
 
     public static boolean isCpf(String cpf) {
         if (StringUtils.isEmpty(cpf) || !cpf.matches("^\\d{11}$") || CPFS_INVALIDOS.contains(cpf))
